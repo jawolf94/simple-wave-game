6e3f8f11bda2e98e24ecb3dfc9f1a2c9c7495e78
jawolf17 <jawolf17@gmail.com>
2019-11-03 15:03:17 -0500

    New Level: Main Menu has been updated to place the player in Level 1 scene as oppoed to Test Level scene. Menu text updated from "Play Test Level" to "Play Game".

---
+++ Third Person Shooter/Assets/Scripts/Game Mangement/Menu Controls/MenuActionPlayGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActionPlayGame : MonoBehaviour, IMenuAction
+{
+    // Constant Scene Paths
+    private const string testScenePath = "Assets/Scenes/Level 1.unity";
+
+    // Load Test Scene on Click
+    public void clickAction()
+    {
+        SceneManager.LoadScene(testScenePath);
+    }
+}
